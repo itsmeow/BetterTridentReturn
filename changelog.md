93fd091ba04e346e0f40249319226c2b51a8e0cc
itsmeow <itsmeow@itsmeow.dev>
2022-01-09 21:53:05 -0600

    Fix crash, set version 1.0.2 (Fixes #2)

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+# 1.0.2
+
+- Fabric: Fix startup crash from mixin
+
 # 1.0.1
 
 - Fixed bug where throwing two tridents from main and offhand at the same time could cause deletion of a trident
--- fabric/src/main/java/dev/itsmeow/bettertridentreturn/mixin/TridentItemMixin.java
+++ fabric/src/main/java/dev/itsmeow/bettertridentreturn/mixin/TridentItemMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TridentItem.class)
 public class TridentItemMixin {
 
-    @Inject(at = @At("HEAD"), method = "releaseUsing(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;I)V")
+    @Inject(at = @At("HEAD"), method = "releaseUsing(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;I)V")
     private void injectReleaseUsing(ItemStack itemStack, Level level, LivingEntity livingEntity, int i, CallbackInfo callback) {
         if(livingEntity instanceof Player) {
             BetterTridentReturnMod.onItemUseFinish((Player) livingEntity, itemStack, i);
